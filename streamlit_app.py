0d7b5f7f441bc190e67c02a504e4accc15558813
kalebtoroitich <110767777+kalebtoroitich@users.noreply.github.com>
2024-04-28 18:05:02 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import matplotlib.pyplot as plt
